863f176bb145e9c9afc8bb3ffe14e444022d048b
Piotr Filipiak <kiddhkane@gmail.com>
2022-08-22 01:06:55 +0200

    Added a right click tooltip.

--- script.js
+++ script.js
@@ -1,10 +1,38 @@
-// alert("bla");
-
 tippy("h1", {
   content: "My tooltip!",
 });
 
 tippy(".tooltips div", {
   content: "bla bla",
-  delay: [100, 100],
+  delay: [50, 50],
+  animateFill: true,
+  //   theme: "light",
+});
+
+const rightClickableArea = document.querySelector(".next-row");
+
+const instance = tippy(rightClickableArea, {
+  content: "Congrats, you can right click. Good for you.",
+  placement: "right-start",
+  trigger: "manual",
+  interactive: true,
+  arrow: false,
+  offset: [0, 0],
+});
+
+rightClickableArea.addEventListener("contextmenu", (event) => {
+  event.preventDefault();
+
+  instance.setProps({
+    getReferenceClientRect: () => ({
+      width: 0,
+      height: 0,
+      top: event.clientY,
+      bottom: event.clientY,
+      left: event.clientX,
+      right: event.clientX,
+    }),
+  });
+
+  instance.show();
 });
